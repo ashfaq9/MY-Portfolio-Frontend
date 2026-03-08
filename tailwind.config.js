b6cf5b9b87e8aacf425896017df9a0f70c031dd8
ashfaq9 <asbaq0@gmail.com>
2026-03-08 13:50:45 +0530

    update vite config and tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   darkMode: 'class',  // ✅ ADD THIS
-//   theme: { extend: {} },
-//   plugins: [],
-// }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',  // ✅ ADD THIS
+  theme: { extend: {} },
+  plugins: [],
+}
 
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    host: true,
-    allowedHosts: ["my-portfolio-frontend-1-estg.onrender.com"]
-  }
-});
